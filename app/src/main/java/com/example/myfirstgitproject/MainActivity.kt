2ae8f9df14f343e9c77b682601ba81e4d44305f8
Innokentiy Trifonov <innocenty.trifonov@gmail.com>
2024-01-02 21:04:46 +0600

    Привет

--- app/src/main/java/com/example/myfirstgitproject/MainActivity.kt
+++ app/src/main/java/com/example/myfirstgitproject/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(modifier: Modifier = Modifier) {
     Text(
-        text = "Дарова",
+        text = "Привет",
         modifier = modifier
     )
 }
