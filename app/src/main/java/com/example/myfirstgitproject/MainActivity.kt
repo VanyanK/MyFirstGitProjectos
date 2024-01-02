dd4f278416f472000fda55dc040a3afad2f0c065
Innokentiy Trifonov <innocenty.trifonov@gmail.com>
2024-01-02 20:40:47 +0600

    test bananas

--- app/src/main/java/com/example/myfirstgitproject/MainActivity.kt
+++ app/src/main/java/com/example/myfirstgitproject/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("test bananas")
+                    Greeting("test bannas")
                 }
             }
         }
